553fac2c2989704252d061f08868812ee164c3b9
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-02-24 00:03:13 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250224/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Y, M, D;
+
+
+bool Month(int a, int b, int c){
+    if(a==2){
+        if(c ==0){
+            if(c>29){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else if(c==1){
+            if(c>28){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+   
+    else if(a==4){
+        if(b >30){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+   
+    else if(a==6){
+        if(b >30){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+   
+    else if(a==9){
+        if(b >30){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+    else if(a==11){
+        if(b >30){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return true;
+    }
+   
+}
+
+string season(int m){
+    if(m >=12 || m <=2){
+        return "Winter";
+    }
+    else if(m >=3 && m <=5){
+        return "Spring";
+    }
+    else if(m >=6 && m <=8){
+        return "Summer";
+    }
+    else if(m >=9 && m <=11){
+        return "Fall";
+    }
+}
+
+int main() {
+    cin >> Y >> M >> D;
+
+    if(Y % 4 == 0 && Y % 100 != 0 || Y % 4 == 0 && Y % 100 == 0 && Y % 400 ==0 ){
+        if(Month(M,D,0)){
+            cout << season(M);
+        }
+        else{
+            cout << -1;
+        }
+
+    }
+    else{
+        if(Month(M,D,1)){
+            cout << season(M);
+        }
+        else{
+            cout << -1;
+        }
+    }
+
+    
+
+
+    return 0;
+}
