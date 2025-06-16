740a5182c2a337e52226d3e397b40819394c9598
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-16 23:06:40 +0900

    Title: 최대 감소 부분 수열, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250616/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int M[1000];
+int dp[1000];
+int answer = 0;
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> M[i];
+        dp[i] = 1;
+    }
+
+
+    for(int i=1; i<N; i++){
+        for(int j=0; j<i; j++){
+            if(M[i] < M[j]){
+                if(dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+    }
+    
+    for(int i=0; i<N; i++){
+        if(answer < dp[i]){
+            answer = dp[i];
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
