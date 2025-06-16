40e63d8428e5c04124db6009da7aa42dca735bfe
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-16 22:58:01 +0900

    Title: 최대 증가 부분 수열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250616/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 250616/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -4,25 +4,33 @@ using namespace std;
 
 int N;
 int M[1000];
-pair<int,int> dp[1000];
+int dp[1000] = {1,};
+int answer = 0;
 
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> M[i];
     }
-    dp[0] = {1,M[0]};
+
 
     for(int i=1; i<N; i++){
-        if(dp[i-1].second >= M[i]){
-            dp[i] = dp[i-1];
-        }
-        else{
-            dp[i] = {(dp[i-1].first)+1 , M[i]};
+        for(int j=0; j<N; j++){
+            if(M[i] > M[j] && i > j){
+                if(dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
         }
     }
     
-    cout << dp[N-1].first;
+    for(int i=0; i<N; i++){
+        if(answer < dp[i]){
+            answer = dp[i];
+        }
+    }
+
+    cout << answer;
 
     return 0;
 }
