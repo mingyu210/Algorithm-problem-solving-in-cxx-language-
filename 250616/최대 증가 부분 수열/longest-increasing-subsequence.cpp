dedd07cdd6ddd496d7c29abc1de1959f25a49790
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-16 23:04:56 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250616/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 250616/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -4,19 +4,20 @@ using namespace std;
 
 int N;
 int M[1000];
-int dp[1000] = {1,};
+int dp[1000];
 int answer = 0;
 
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> M[i];
+        dp[i] = 1;
     }
 
 
     for(int i=1; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(M[i] > M[j] && i > j){
+        for(int j=0; j<i; j++){
+            if(M[i] > M[j]){
                 if(dp[i] < dp[j] + 1){
                     dp[i] = dp[j] + 1;
                 }
