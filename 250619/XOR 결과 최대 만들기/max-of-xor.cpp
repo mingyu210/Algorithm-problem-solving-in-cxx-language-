164519c3b9258d6cdb882deea1fbfe66b205bd22
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-19 11:32:08 +0900

    Title: XOR 결과 최대 만들기, Time: 1050ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 250619/XOR 결과 최대 만들기/max-of-xor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int A[20];
+int answer[20];
+bool visited[20];
+int answer2 = 0;
+
+
+
+
+
+void backTracking(int num){
+    if(num == m){
+        int result = answer[0];
+        for(int i=1; i<m; i++){
+            result ^= answer[i];
+        }
+        if(answer2 < result){
+            answer2 = result;
+        }
+    }
+    else{
+        for(int i=0; i<n; i++){
+        if(!visited[i]){
+            answer[num] = A[i];
+        }
+        backTracking(num+1);
+        visited[i] = false;
+    }
+    }
+   
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    backTracking(0);
+    cout << answer2;
+    
+
+    return 0;
+}
