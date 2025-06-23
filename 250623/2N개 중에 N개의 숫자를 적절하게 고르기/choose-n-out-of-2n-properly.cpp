3dc7634c2ff6e68f3519fa0c00d53d49d3b8b7a6
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-23 09:51:27 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250623/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.cpp
+++ 250623/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.cpp
@@ -8,7 +8,29 @@ int n;
 int num[20];
 int sum = 0;
 int minSum = 0;
-int answer=0;
+int answer=1e9;
+bool visited[20];
+
+
+void backTracking(int start ,int num2){
+    if(num2 == n){
+        if(answer > abs(sum - minSum)){
+            answer = abs(sum - minSum);
+        }
+    }
+
+    for(int i=start; i<2*n; i++){
+        if(!visited[i]){
+            minSum += num[i];
+            sum -= num[i];
+            visited[i] = true;
+            backTracking(i+1,num2+1);
+            minSum -= num[i];
+            sum += num[i];
+            visited[i] = false;
+        }
+    }
+}
 
 int main() {
     cin >> n;
@@ -16,30 +38,8 @@ int main() {
         cin >> num[i];
         sum += num[i];
     }
-
-    sort(num, num+2*n);
-
-    for(int i=0; i<n; i++){
-        if(i == n-1){
-            if((minSum + num[i]) - (sum - num[i]) > (minSum + num[2*n-i-1]) - (sum - num[2*n-i-1])){
-                minSum += num[n-i-1];
-                sum -= num[2*n-i-1];
-            }
-            else{
-                minSum += num[i];
-                sum -= num[i];
-            }
-        }
-        else{
-            minSum += num[i];
-            sum -= num[i];
-            minSum += num[2*n-i-1];
-            sum -= num[2*n-i-1];
-            i++;
-        }
-    }
-    
-    cout << abs(minSum - sum);
+    backTracking(0,0);
+    cout << answer;
 
     return 0;
 }
