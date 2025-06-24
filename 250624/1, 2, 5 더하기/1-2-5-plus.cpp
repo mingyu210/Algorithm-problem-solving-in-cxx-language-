6d8aede7e49a9984b195c9c03b6c1101e711d116
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-24 10:32:42 +0900

    Title: 1, 2, 5 더하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250624/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+    cin >> n;
+    dp[0]=0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    dp[4] = 5;
+    dp[5] = 9;
+    
+    for(int i=6; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-5];
+    }
+
+    cout << dp[n];
+    
+
+    return 0;
+}
