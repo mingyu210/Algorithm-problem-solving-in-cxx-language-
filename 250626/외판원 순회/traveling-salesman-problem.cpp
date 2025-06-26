97a0f3eb8afca2e94950dbff69d47ddf55ec4a0b
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-26 09:14:13 +0900

    Title: 외판원 순회, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250626/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int n;
+int A[10][10];
+bool visited[11];
+vector<int> answer;
+int realAnswer = INT_MAX;
+
+void backTracking(int start , int num){
+    if(num == n){
+        int minAnswer = 0;
+        answer.push_back(A[start][1]);
+        for(auto ac: answer){
+            minAnswer += ac;
+        }
+        if(minAnswer < realAnswer){
+            realAnswer = minAnswer;
+        }
+        answer.pop_back();
+    }
+    else{
+        for(int i=1; i<=n; i++){
+            if(start != i && !visited[i] && A[start][i]!=0){
+                answer.push_back(A[start][i]);
+                visited[i] = true;
+                backTracking(i,num+1);
+                answer.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    visited[1] = true;
+    backTracking(1,1);
+    cout << realAnswer;
+
+    return 0;
+}
