3421cec43a60c909c521756ed4179742ae3d0fca
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-27 11:12:00 +0900

    Title: 안전 지대, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250627/안전 지대/comfort-zone.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int N, M;
+int grid[50][50];
+int bigK;
+int answerK;
+int answer;
+bool visited[50][50];
+
+void dfs2(int k, int x, int y){
+    int dx[2] = {0,1};
+    int dy[2] = {1,0};
+
+    visited[x][y] = true;
+
+    for(int i=0; i<2; i++){
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if(new_x>=0 && new_x < N && new_y>=0 && new_y < M && !visited[new_x][new_y] && grid[new_x][new_y] > k){
+            dfs2(k,new_x,new_y);
+        }
+    }
+
+
+}
+
+
+int dfs(int k){
+    int num = 0;
+    for(int i=0; i<N; i++){
+        for(int j =0; j<M; j++){
+            if(!visited[i][j] && grid[i][j] > k){
+                num++;
+                dfs2(k,i,j);
+            }
+        }
+    }
+    return num;
+}   
+
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> grid[i][j];
+            bigK = max(grid[i][j] , bigK);
+        }
+    }
+
+    for(int i=1; i<bigK; i++){
+        int cq = dfs(i);
+        if(answer < cq){
+            answer = cq;
+            answerK = i;
+        }
+        memset(visited, 0, sizeof(visited));
+    }
+
+    cout << answerK << " " << answerK;
+
+    return 0;
+}
